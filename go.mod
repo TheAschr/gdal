a23f21b6efad14776c5e1aa72f38b58f367f70d4
Alex Schrimpf <aschrimpf@igskrdcnvmw2048.er.usgs.gov>
2023-08-10 15:43:43 +0000

    Added IsFieldSetAndNotNull

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/lukeroth/gdal
 go 1.15
 
 require github.com/stretchr/testify v1.7.2 // indirect
+
+replace github.com/lukeroth/gdal => github.com/TheAschr/gdal v1.0.0
--- ogr.go
+++ ogr.go
@@ -314,7 +314,7 @@ type Geometry struct {
 	cval C.OGRGeometryH
 }
 
-//Create a geometry object from its well known binary representation
+// Create a geometry object from its well known binary representation
 func CreateFromWKB(wkb []uint8, srs SpatialReference, bytes int) (Geometry, error) {
 	cString := unsafe.Pointer(&wkb[0])
 	var newGeom Geometry
@@ -323,7 +323,7 @@ func CreateFromWKB(wkb []uint8, srs SpatialReference, bytes int) (Geometry, erro
 	).Err()
 }
 
-//Create a geometry object from its well known text representation
+// Create a geometry object from its well known text representation
 func CreateFromWKT(wkt string, srs SpatialReference) (Geometry, error) {
 	cString := C.CString(wkt)
 	defer C.free(unsafe.Pointer(cString))
@@ -333,7 +333,7 @@ func CreateFromWKT(wkt string, srs SpatialReference) (Geometry, error) {
 	).Err()
 }
 
-//Create a geometry object from its GeoJSON representation
+// Create a geometry object from its GeoJSON representation
 func CreateFromJson(_json string) Geometry {
 	cString := C.CString(_json)
 	defer C.free(unsafe.Pointer(cString))
@@ -1209,6 +1209,12 @@ func (feature Feature) FieldIndex(name string) int {
 // Return if a field has ever been assigned a value
 func (feature Feature) IsFieldSet(index int) bool {
 	set := C.OGR_F_IsFieldSet(feature.cval, C.int(index))
+	return set != 0
+}
+
+// Test if a field is set and not null.
+func (feature Feature) IsFieldSetAndNotNull(index int) bool {
+	set := C.OGR_F_IsFieldSetAndNotNull(feature.cval, C.int(index))
 	return set != 0
 }
 
