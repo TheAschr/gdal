034ce062e0696d9c54af1be7b225f2f22f409911
Schrimpf <aschrimpf@contractor.usgs.gov>
2025-05-20 14:05:52 -0500

    Fixed

--- osr.go
+++ osr.go
@@ -67,11 +67,6 @@ func (sr SpatialReference) ToPrettyWKT(simplify bool) (string, error) {
 	return wkt, err
 }
 
-func (sr SpatialReference) GetEPSGGeogCS() int {
-	code := C.OSREPSGGeogCS(sr.cval)
-	return int(code)
-}
-
 // Initialize SRS based on EPSG code
 func (sr SpatialReference) FromEPSG(code int) error {
 	cErr := C.OSRImportFromEPSG(sr.cval, C.int(code))
